5e14672e7aca2fad9ee5eaefbd6fa54dffb8048a
Aurélien Roux <aureroux13@gmail.com>
2018-10-27 16:38:49 -0400

    add boolean file

---
+++ basics/boolean.js
@@ -0,0 +1,18 @@
+let temp = -30
+
+if (temp < 0) {
+  console.log('freezing');
+} else {
+  console.log('not freezing');
+}
+
+
+let age = 7
+
+if (age <= 7) {
+  console.log('child discount');
+}
+
+if (age >= 65) {
+  console.log('senior discount');
+}
