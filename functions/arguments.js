164ca374966fb838a9c01452151232b983ddcb8a
Aurélien Roux <aureroux13@gmail.com>
2018-10-28 10:59:14 -0400

    functions files

---
+++ functions/arguments.js
@@ -0,0 +1,18 @@
+let getScore = function(name = 'Aure', score = 0) {
+  return `Player ${name} has ${score} points`
+}
+
+let result = getScore(undefined, 100)
+console.log(result);
+
+let tip = function(amount, tip = .2) {
+  return amount + (amount * tip)
+}
+
+let billOne = tip(20)
+let billTwo = tip(15, .1)
+let billThree = tip(10, .3)
+
+console.log(billOne);
+console.log(billTwo);
+console.log(billThree);
---
+++ functions/functions-101.js
@@ -0,0 +1,15 @@
+let tempConv = function (temp) {
+  return (temp - 32) * 5 / 9
+}
+
+let result = tempConv(32)
+let otherResult = tempConv(68)
+
+console.log(result);
+console.log(otherResult);
+
+let age = 25
+
+age = null
+
+console.log(age);
