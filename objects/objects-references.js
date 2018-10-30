c36170b23e9b451b485f83bad7d06b1224c6b024
Aurélien Roux <aureroux13@gmail.com>
2018-10-29 20:42:49 -0400

    object expense tracker

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -4,11 +4,27 @@ let myAccount = {
   income: 0
 }
 
-let addExpenses = function(account, amount) {
+let addExpense = function(account, amount) {
   account.expenses = account.expenses + amount
-  console.log(account);
 }
 
-addExpenses(myAccount, 2.5);
+let addIncome = function(account, amount) {
+  account.income = account.income + amount
+}
 
-console.log(myAccount);
+let resetAccount = function(account) {
+  account.expenses = 0
+  account.income = 0
+}
+
+let getAccountSummary = function(account) {
+  let balance = account.income - account.expenses
+  return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses`
+}
+
+addIncome(myAccount, 100)
+addExpense(myAccount, 2.5)
+addExpense(myAccount, 52.5)
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount));
