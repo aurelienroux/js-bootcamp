22c9860f3b02a928c1c69299337f36a6abdd0e66
Aurélien Roux <aureroux13@gmail.com>
2018-10-29 20:28:39 -0400

    object references file

---
+++ objects/objects-references.js
@@ -0,0 +1,14 @@
+let myAccount = {
+  name: "john",
+  expenses: 0,
+  income: 0
+}
+
+let addExpenses = function(account, amount) {
+  account.expenses = account.expenses + amount
+  console.log(account);
+}
+
+addExpenses(myAccount, 2.5);
+
+console.log(myAccount);
