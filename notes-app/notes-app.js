082c00acc22fae0b5da51467d59d36d7af9f436c
Aurélien Roux <aureroux13@gmail.com>
2018-11-05 20:50:58 -0500

    dom manipulation on notes files

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,12 @@
-console.log('test');
+const p = document.querySelector('p')
+console.log(p);
+
+const allP = document.querySelectorAll('p')
+console.log(allP);
+
+allP.forEach(function(p) {
+  console.log(p.textContent)
+  p.textContent = 'changed'
+  console.log(p.textContent)
+  // p.remove()
+})
