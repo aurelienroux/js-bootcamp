c1b2ee5c5187c17a8bf8c6e1b0c6129413020491
Aurélien Roux <aureroux13@gmail.com>
2018-11-07 20:45:12 -0500

    counting remaining todos

--- todos-app/todos-app.js
+++ todos-app/todos-app.js
@@ -1,7 +1,31 @@
-const paragraphs = document.querySelectorAll('p')
+const todos = [{
+  text: 'Order food',
+  completed: false
+}, {
+  text: 'clean room',
+  completed: true
+}, {
+  text: 'exercice',
+  completed: false
+}, {
+  text: 'do work',
+  completed: true
+}, {
+  text: 'buy food',
+  completed: false
+}]
 
-paragraphs.forEach(function (paragraph) {
-  if (paragraph.textContent.includes('the')) {
-    paragraph.remove()
-  }
+const uncompletedTodos = todos.filter(function(todo) {
+  return !todo.completed;
+})
+
+const summary = document.createElement('h3')
+summary.textContent = `You have ${uncompletedTodos.length} todos left`
+document.body.appendChild(summary)
+
+todos.forEach(function(todo, index) {
+  todoTag = document.createElement('p')
+  todoPosition = index + 1
+  todoTag.textContent = `${todoPosition}. ${todo.text}`
+  document.body.appendChild(todoTag)
 })
