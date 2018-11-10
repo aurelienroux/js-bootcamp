9cbe8e5349dce613a2aff78110d92bb1c6237517
Aurélien Roux <aureroux13@gmail.com>
2018-11-09 23:34:49 -0500

    notes active filter input method

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,5 +1,5 @@
 const notes = [{
-  title: 'My next trip',
+  title: 'My next trip at',
   body: 'I would like to go to Spain'
 }, {
   title: 'Habbits to work on',
@@ -8,6 +8,23 @@ const notes = [{
   title: 'Office modification',
   body: 'get a new seat'
 }]
+
+const renderNotes = function (notes, searchTerm) {
+  const filteredNotes = notes.filter(function (note) {
+    return note.title.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  const notesContainer = document.querySelector('#notes-container')
+  notesContainer.innerHTML = ''
+
+  filteredNotes.forEach(function (note) {
+    const noteEl = document.createElement('p')
+    noteEl.textContent = note.title
+    notesContainer.appendChild(noteEl)
+  })
+}
+
+renderNotes(notes, '')
 
 document.querySelector('#remove-all').addEventListener('click', function () {
   document.querySelectorAll('.notes').forEach(function (note) {
@@ -19,6 +36,6 @@ document.querySelector('#create-note').addEventListener('click', function () {
   console.log('create note');
 })
 
-document.querySelector('#search-text').addEventListener('input', function(e) {
-  console.log(e.target.value);
+document.querySelector('#search-text').addEventListener('input', function (e) {
+  renderNotes(notes, e.target.value)
 })
