e7df86d6b486c891e698938216e217ea24087bc8
Aurélien Roux <aureroux13@gmail.com>
2018-11-25 14:46:02 -0500

    type coercion file

---
+++ advanced/type-coercion.js
@@ -0,0 +1,16 @@
+// Type coercion - a string, a number , a boolean
+
+console.log('5' + 5) // 55
+console.log('5' - 5) // 0
+console.log('5' == 5) // true
+console.log('5' === 5) // false
+
+const typeN = typeof 123
+console.log(typeN) // number
+
+const value = false + 12 // true === 1 & false === 0
+const type = typeof value
+console.log(type)
+console.log(value)
+
+
