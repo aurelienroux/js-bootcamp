347b569e684ff357e77166f1ddaa2e0015744dbe
Aurélien Roux <aureroux13@gmail.com>
2018-12-05 20:58:19 -0500

    create methods on proto object

--- oop/person.js
+++ oop/person.js
@@ -1,10 +1,28 @@
-const Person = function (firstName, lastName, age) {
+const Person = function (firstName, lastName, age, likes = []) {
   this.firstName = firstName
   this.lastName = lastName
   this.age = age
+  this.likes = likes
 }
 
-const me = new Person('aure', 'roux', 37)
+Person.prototype.getBio = function () {
+  bio = `${this.firstName} ${this.lastName} is ${this.age}. `
 
-console.log(me);
-console.log(me.summary());
+  this.likes.forEach(like => {
+    bio += ` ${this.firstName} likes ${like}.`
+  });
+
+  return bio
+}
+
+Person.prototype.setName = function(fullName) {
+  const names = fullName.split(' ')
+  this.firstName = names[0]
+  this.lastName = names[1]
+}
+
+const me = new Person('Aure', 'Roux', 37, ['fitness', 'guitar'])
+const Dadou = new Person('Dadou', 'Roux', 38)
+
+console.log(me.getBio());
+console.log(Dadou.getBio());
