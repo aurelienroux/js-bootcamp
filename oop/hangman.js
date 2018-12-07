2e94abd4f070a88815e10cc7c3a9464e0baf995d
Aurélien Roux <aureroux13@gmail.com>
2018-12-06 20:34:05 -0500

    hangman get puzzle method

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,33 @@
-const Hangman = function(word, remaininGuesses) {
-  this.word = word
+const Hangman = function (word, remaininGuesses) {
+  this.lettersArray = word.toLowerCase().split('')
   this.remaininGuesses = remaininGuesses
+  this.guessedLetters = []
 }
 
-const gameOne = new Hangman('computer', 10)
-const gameTwo = new Hangman('javascript', 7)
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = ''
+
+  this.lettersArray.forEach(letter => {
+    if (this.guessedLetters.includes(letter) || letter === ' ') {
+      puzzle += letter
+    } else {
+      puzzle += '*'
+    }
+  });
+  return puzzle
+}
+
+Hangman.prototype.guessOne = function (guessedLetter) {
+  this.guessedLetters.push(guessedLetter)
+  this.remaininGuesses = this.remaininGuesses - 1
+
+  return this.getPuzzle()
+}
+
+const gameOne = new Hangman('javascript', 10)
+const gameTwo = new Hangman('React Dom', 10)
 
 console.log(gameOne);
-console.log(gameTwo);
+console.log(gameOne.getPuzzle());
+console.log(gameOne.guessOne('a'));
+
