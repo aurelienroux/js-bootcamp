b863a2bb84681a9aaae195a75a2cb849a44ca4f4
Aurélien Roux <aureroux13@gmail.com>
2018-12-12 21:26:22 -0500

    convert HangMan and Person to class syntaxes

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,66 +1,60 @@
-const Hangman = function (word, remaininGuesses) {
-  this.lettersArray = word.toLowerCase().split('')
-  this.remaininGuesses = remaininGuesses
-  this.guessedLetters = []
-  this.status = 'playing'
-  this.message = `Playing. Guesses left ${this.remaininGuesses}`
-}
+class Hangman {
+  constructor(word, remaininGuesses) {
+    this.lettersArray = word.toLowerCase().split('')
+    this.remaininGuesses = remaininGuesses
+    this.guessedLetters = []
+    this.status = 'playing'
+    this.message = `Playing. Guesses left ${this.remaininGuesses}`
+  }
 
-Hangman.prototype.getPuzzle = function () {
-  let puzzle = ''
+  getPuzzle() {
+    let puzzle = ''
+    this.lettersArray.forEach(letter => {
+      if (this.guessedLetters.includes(letter) || letter === ' ') {
+        puzzle += letter
+      } else {
+        puzzle += '*'
+      }
+    });
+    return puzzle
+  }
 
-  this.lettersArray.forEach(letter => {
-    if (this.guessedLetters.includes(letter) || letter === ' ') {
-      puzzle += letter
-    } else {
-      puzzle += '*'
+  makeGuess(guessedLetter) {
+    guessedLetter = guessedLetter.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(guessedLetter);
+    const isBadGuess = !this.lettersArray.includes(guessedLetter);
+    if (this.status !== 'playing') {
+      return;
     }
-  });
-  return puzzle
-}
-
-Hangman.prototype.makeGuess = function (guessedLetter) {
-  guessedLetter = guessedLetter.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guessedLetter);
-  const isBadGuess = !this.lettersArray.includes(guessedLetter);
-
-  if (this.status !== 'playing') {
-    return;
+    if (isUnique) {
+      this.guessedLetters.push(guessedLetter)
+    }
+    if (isUnique && isBadGuess) {
+      this.remaininGuesses--
+    }
+    this.statusUpdate()
+    this.printMessage()
   }
 
-  if (isUnique) {
-    this.guessedLetters.push(guessedLetter)
+  statusUpdate() {
+    const win = this.lettersArray.every((letter) => {
+      return this.guessedLetters.indexOf(letter) > -1
+    })
+    if (win && this.remaininGuesses >= 1) {
+      this.status = 'finished'
+    }
+    if (this.remaininGuesses === 0) {
+      this.status = 'failed'
+    }
   }
 
-  if (isUnique && isBadGuess) {
-    this.remaininGuesses--
-  }
-
-  this.statusUpdate()
-  this.printMessage()
-}
-
-Hangman.prototype.statusUpdate = function () {
-  const win = this.lettersArray.every((letter) => {
-    return this.guessedLetters.indexOf(letter) > -1
-  })
-
-  if (win && this.remaininGuesses >= 1) {
-    this.status = 'finished'
-  }
-
-  if (this.remaininGuesses === 0) {
-    this.status = 'failed'
-  }
-}
-
-Hangman.prototype.printMessage = function () {
-
-  if (this.status === 'playing') {
-    return `Playing. guesses left: ${this.remaininGuesses}`
-  } else if (this.status === 'failed') {
-    return `Nice try. word was "${this.lettersArray.join('')}"`
-  } else {
-    return `Great!`
+  printMessage() {
+    if (this.status === 'playing') {
+      return `Playing. guesses left: ${this.remaininGuesses}`
+    } else if (this.status === 'failed') {
+      return `Nice try. word was "${this.lettersArray.join('')}"`
+    } else {
+      return `Great!`
+    }
   }
 }
--- oop/person.js
+++ oop/person.js
@@ -1,26 +1,23 @@
-const Person = function (firstName, lastName, age, likes = []) {
-  this.firstName = firstName
-  this.lastName = lastName
-  this.age = age
-  this.likes = likes
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+  }
+  getBio() {
+    let bio = `${this.firstName} ${this.lastName} is ${this.age}. `
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}.`
+    });
+    return bio
+  }
+  setName(fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+  }
 }
-
-Person.prototype.getBio = function () {
-  bio = `${this.firstName} ${this.lastName} is ${this.age}. `
-
-  this.likes.forEach(like => {
-    bio += ` ${this.firstName} likes ${like}.`
-  });
-
-  return bio
-}
-
-Person.prototype.setName = function(fullName) {
-  const names = fullName.split(' ')
-  this.firstName = names[0]
-  this.lastName = names[1]
-}
-
 const me = new Person('Aure', 'Roux', 37, ['fitness', 'guitar'])
 const Dadou = new Person('Dadou', 'Roux', 38)
 
