29eaedb94748eea6761377459816da6f3862ad04
Aurélien Roux <aureroux13@gmail.com>
2018-12-16 16:18:05 -0500

    callback pattern on get puzzle api call

--- hangman/app.js
+++ hangman/app.js
@@ -8,34 +8,28 @@ guessesNode.textContent = game1.statusMessage
 window.addEventListener('keypress', function (e) {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess)
-
   puzzleNode.textContent = game1.puzzle
   guessesNode.textContent = game1.statusMessage
 })
 
-
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    console.log(data.puzzle);
-  } else if (e.target.readyState === 4) {
-    console.log('error');
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(puzzle);
   }
 })
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
 
 
-const countryRequest = new XMLHttpRequest()
-countryRequest.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    country = data.find(el => el.alpha2Code === 'CA')
-    console.log(country.name);
-  } else if (e.target.readyState === 4) {
-    console.log('unable to fecth data');
-  }
-})
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-countryRequest.send()
+// const countryRequest = new XMLHttpRequest()
+// countryRequest.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     country = data.find(el => el.alpha2Code === 'CA')
+//     console.log(country.name);
+//   } else if (e.target.readyState === 4) {
+//     console.log('unable to fecth data');
+//   }
+// })
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
