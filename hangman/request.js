154f603ff19df6e94dbc0d4397cb59d8790a4e20
Aurélien Roux <aureroux13@gmail.com>
2019-01-06 12:48:49 -0500

    fetch methods async await

--- hangman/request.js
+++ hangman/request.js
@@ -8,24 +8,21 @@ const getPuzzle = async (wordCount) => {
   }
 }
 
-const getCountry = (countryCode) => {
-  return fetch(`http://restcountries.eu/rest/v2/all`).then((resp) => {
-    if (resp.status === 200) {
-      return resp.json()
-    } else {
-      throw new Error('unable to fetch country')
-    }
-  }).then((data) => {
+const getCountry = async (countryCode) => {
+  const response = await fetch(`http://restcountries.eu/rest/v2/all`)
+  if (response.status === 200) {
+    const data = await response.json()
     return data.find(el => el.alpha2Code === countryCode)
-  })
+  } else {
+    throw new Error('unable to get country')
+  }
 }
 
-const getIpAdress = (token) => {
-  return fetch(`https://ipinfo.io/json?token=${token}`).then((resp) => {
-    if (resp.status === 200) {
-      return resp.json()
-    } else {
-      throw new Error('unable to fetch ip address')
-    }
-  })
+const getIpAdress = async (token) => {
+  const response = await fetch(`https://ipinfo.io/json?token=${token}`)
+  if (response.status === 200) {
+    return response.json()
+  } else {
+    throw new Error('unable to fetch ip address')
+  }
 }
