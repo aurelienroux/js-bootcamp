7086766dc547060c2a8ec34c463999c96f8ed069
Aurélien Roux <aureroux13@gmail.com>
2019-01-13 15:31:06 -0500

    add webpack server and sourcemap

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -2,7 +2,7 @@ import squareDefault, { add, name } from './utilities'
 import otherScream from "./scream";
 
 console.log('index');
-console.log(add(3,5));
+console.log(add(10,5));
 console.log('index', name);
 console.log(otherScream('scream function'));
 console.log('square', squareDefault(3));
