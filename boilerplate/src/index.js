4df528f37502b48061664e4dfe070f16c4cbd354
Aurélien Roux <aureroux13@gmail.com>
2019-01-23 21:22:43 -0500

    object spread operator

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -13,3 +13,37 @@ citiesBis = ['Nantes', ...cities]
 
 console.log(cities);
 console.log(citiesBis);
+
+
+let house = {
+  bedrooms: 2,
+  bathrooms: 1.5,
+  yearBuilt: 2017
+}
+let newHouse = {
+  basement: true,
+  ...house,
+  yearBuilt: 2020
+}
+console.log(house);
+console.log(newHouse);
+
+
+const person = {
+  name: 'Aure',
+  age: 37
+}
+
+const location = {
+  city: 'Montreal',
+  country: 'Canada'
+}
+
+const completePerson = {
+  ...person,
+  ...location
+}
+
+console.log(person);
+console.log(location);
+console.log(completePerson);
